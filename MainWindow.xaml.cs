143f0c4a5c4e5e8719f3765b1fc7b41542cb4a5c
Merger Oksana <oksana_kiw@mail.ru>
2014-06-04 19:38:31 +0400

    First Commit
    Just start to write this one

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+namespace DevCor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {}
+
+
+        private void rectangle1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void rectangle2_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            var dw = new DesktopWindow();
+            dw.Show();
+            Close();
+        }
+    }
+}
+
---
+++ WaterMark.cs
@@ -0,0 +1,61 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+
+namespace DevCor
+{
+    class WaterMark : DependencyObject
+    {
+        public static readonly DependencyProperty TextLengthProperty = DependencyProperty.RegisterAttached("TextLength", typeof(int), typeof(WaterMark), new UIPropertyMetadata(0));
+        private static readonly DependencyProperty HasTextProperty = DependencyProperty.RegisterAttached("HasText", typeof(bool), typeof(WaterMark), new FrameworkPropertyMetadata(false));
+        public static readonly DependencyProperty IsMonitoringProperty = DependencyProperty.RegisterAttached("IsMonitoring", typeof(bool), typeof(WaterMark), new UIPropertyMetadata(false, OnIsMonitoringChanged));
+        public static bool GetIsMonitoring(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsMonitoringProperty);
+        }
+
+        public static void SetIsMonitoring(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsMonitoringProperty, value);
+        }
+
+        public bool HasText
+        {
+            get { return (bool)GetValue(HasTextProperty); }
+            set { SetValue(HasTextProperty, value); }
+        }
+
+        static void OnIsMonitoringChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox passB = d as PasswordBox;
+            if ((bool)e.NewValue)
+                passB.PasswordChanged += PasswordChanged;
+            else
+                passB.PasswordChanged -= PasswordChanged;
+
+        }
+
+        static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passB = sender as PasswordBox;
+            if (passB == null)
+                return;
+            SetTextLength(passB, passB.Password.Length);
+        }
+        public static void SetTextLength(DependencyObject obj, int value)
+        {
+            obj.SetValue(TextLengthProperty, value);
+
+            if (value >= 1)
+                obj.SetValue(HasTextProperty, true);
+            else
+                obj.SetValue(HasTextProperty, false);
+        }
+
+        public static int GetTextLength(DependencyObject obj)
+        {
+            return (int)obj.GetValue(TextLengthProperty);
+        }
+
+    }
+}
